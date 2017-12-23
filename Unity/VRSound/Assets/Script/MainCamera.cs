cd36067fff84f5771fa2a06d7780f8a32de138a0
admin <sekibotbot@gmail.com>
2017-12-23 16:57:14 +0900

    modify way of camera move

--- Unity/VRSound/Assets/Script/MainCamera.cs
+++ Unity/VRSound/Assets/Script/MainCamera.cs
@@ -14,11 +14,16 @@ public class MainCamera : MonoBehaviour {
 
 
 
+
 	// Use this for initialization
 	void Start () {
 		step_num = CalculateInnerPoint.step_num;
 		u_array = new float[step_num];
 		aud = GetComponent<AudioSource> ();
+
+
+
+
 	}
 	
 	// Update is called once per frame
@@ -41,32 +46,42 @@ public class MainCamera : MonoBehaviour {
 			aud = GetComponent<AudioSource> ();
 //			aud.Stop ();
 		}
+			
+		Quaternion cam_forward = this.transform.rotation;
+		Vector3 vec = cam_forward.eulerAngles;
+		Vector3 vec_forward = new Vector3(vec.x,vec.z,vec.y);
+		Vector3 vec_side = new Vector3 (vec.y, vec.x, vec.z);
+//		print (cam_forward);
 
-		Vector3 v = this.transform.localPosition;
+
+
+//		Vector3 v = this.transform.localPosition;
 		Vector3 l = this.transform.localEulerAngles;
-
+//
 		// カメラの方向から、X-Z平面の単位ベクトルを取得
-		Vector3 cameraForward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1)).normalized;
+//		Vector3 cameraForward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1)).normalized;
 
 		if (Input.GetKey(KeyCode.W)) {   // Wキーで前進.
-			v.z += 1f;
-//			aud.Stop ();
-//			aud.clip = myClip;
-//			aud.Play();
+//			v.z += 1f;
+			this.transform.Translate (vec_forward.normalized); 
 		}
 		if (Input.GetKey(KeyCode.Z)) {   // Sキーで後退.
-			v.z -= 1f;
+//			v.z -= 1f;
+			this.transform.Translate (-vec_forward.normalized); 
 		}
 		if (Input.GetKey(KeyCode.A)) {  // Aキーで左移動.
-			v.x -= 1f;
+//			v.x -= 1f;
+			this.transform.Translate (-vec_side.normalized);  
 		}
 		if (Input.GetKey(KeyCode.S)) {  // Dキーで右移動.
-			v.x += 1f;
+//			v.x += 1f;
+			this.transform.Translate (vec_side.normalized);  
 		}
-		this.transform.localPosition = v;
-
+////		this.transform.localPosition = v;
+//
 		if (Input.GetKey(KeyCode.UpArrow)) {  // 上矢印キーで上をむく移動.
 			l.x += -1f;
+//			this.transform.Rotate (new Vector3(0, 0, -1f));
 		}
 		if (Input.GetKey(KeyCode.DownArrow)) {  // 下矢印キーでしたをむく移動.
 			l.x += 1f;
@@ -96,7 +111,7 @@ public class MainCamera : MonoBehaviour {
 		int count = 0;
 		while (count < CalculateInnerPoint.u_array.Length)//data.Length)
 		{	
-			data [count] = CalculateInnerPoint.u_array [count];//0.3f * Mathf.Sign (Mathf.Sin (2 * Mathf.PI * frequency * position / samplerate));// u_array [count];
+			data [count] = 0.3f*CalculateInnerPoint.u_array [count];//0.3f * Mathf.Sign (Mathf.Sin (2 * Mathf.PI * frequency * position / samplerate));// u_array [count];
 			position++;
 			count++;
 		}
